86ccd5c710dd9034015c9f92e6ee9044d66c3f61
annuk14 <annu140728@gmail.com>
2024-02-29 23:23:08 +0530

    javaprograms

---
+++ InterviewQ1.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+
+
+public class InterviewQ1 {
+
+	/**
+	 *
+       print a1 b2 c3 d4 e5 ......y25 z26
+ */
+	
+	public static void main(String[] args) {
+
+		int count=0;
+		for(char c='a';c<='z';c++) {
+			count++;
+		System.out.print(c+""+count+" ");
+	}
+			
+	}
+}
+
+
